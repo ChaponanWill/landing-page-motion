55d0103fc6c1d04c1593643572a403efb1123954
chaponanwill <chapowilliam09@gmail.com>
2024-10-07 18:05:58 -0500

    name website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Poppins } from 'next/font/google'
 const popinsFont = Poppins({ weight: ['400', '600', '700'], subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Bank Website | TarreDev 💸 ',
+  title: 'Bank Website | TarreDev 💸 & ChaponanWill ',
   description: 'Follow me for more!',
 }
 
